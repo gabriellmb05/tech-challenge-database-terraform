3b4608c44a65c6c591b0233a79fea9575e46639e
gabriellmb05 <gabriel.luiz.mb@gmail.com>
2025-05-15 14:17:43 -0300

    Definindo key_prefix

--- infra/dataremote.tf
+++ infra/dataremote.tf
@@ -5,5 +5,6 @@ data "terraform_remote_state" "vpc" {
     bucket = "terraform-chanllenge-tf"
     key    = "state/terraform.tfstate"
     region = "us-east-1"
+    workspace_key_prefix = var.environment
   }
 }
